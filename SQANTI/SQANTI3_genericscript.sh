34a15fd150a5ae1e606fdec3d7db4bfefc74c08a
efwatts <141367442+efwatts@users.noreply.github.com>
2024-02-19 08:51:59 -0500

    Create SQANTI3_genericscript.sh

---
+++ SQANTI/SQANTI3_genericscript.sh
@@ -0,0 +1,9 @@
+python sqanti3_qc.py \
+merged.collapsed.gff \
+annotated_genome.gtf \
+assembly_genome.fasta \
+--skipORF \
+-o output_title \
+-d SQANTI3_out/ \
+--fl_count merged.collapsed.abundance.txt  \
+--gtf
