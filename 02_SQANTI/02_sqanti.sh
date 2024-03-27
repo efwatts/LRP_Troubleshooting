837ddf2357e0652eca0e83147381363d5206f1c4
Emily F. Watts <watts.emily.f@gmail.com>
2024-03-27 11:14:58 -0400

    Add files via upload

---
+++ 02_SQANTI/02_sqanti.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --job-name=SQANTI3
+#SBATCH --cpus-per-task=10 #number of cores to use
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=12:00:00 #amount of time for the whole job
+#SBATCH --partition=standard #the queue/partition to run on
+#SBATCH --account=sheynkman_lab
+#SBATCH --output=%x-%j.log
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=yqy3cu@virginia.edu
+
+module purge 
+module load gcc/11.4.0
+module load openmpi/4.1.4
+module load R/4.3.1 
+module load python/3.11.4 
+module load anaconda/2023.07-py3.11 
+module load perl/5.36.0 
+module load star/2.7.9a 
+module load kallisto/0.48.0
+
+cd /project/sheynkman/users/emily/LRP_test/jurkat
+
+conda activate SQANTI3.env
+
+python ./02_sqanti/SQANTI3-5.2/sqanti3_qc.py \
+./01_isoseq/collapse/merged.collapsed.gff \
+./00_input_data/gencode.v35.annotation.canonical.gtf \
+./00_input_data/GRCh38.primary_assembly.genome.fa \
+--skipORF \
+-o jurkat \
+-d ./02_sqanti/output/ \
+--fl_count ./01_isoseq/collapse/merged.collapsed.abundance.txt  
+
+
+conda deactivate
