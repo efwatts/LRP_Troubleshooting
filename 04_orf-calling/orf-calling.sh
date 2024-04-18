0243866edcf3cc4e490c5f6cf541aba9ae8c5055
Emily F. Watts <watts.emily.f@gmail.com>
2024-04-18 15:27:00 -0400

    Update orf-calling.sh

--- 04_orf-calling/orf-calling.sh
+++ 04_orf-calling/orf-calling.sh
@@ -1,4 +1,4 @@
-##trying on Rivanna
+```
 module load apptainer/1.2.2
 module load gcc/11.4.0  
 module load openmpi/4.1.4
@@ -6,40 +6,30 @@ module load python/3.11.4
 module load bioconda/py3.10
 
 apptainer pull docker://gsheynkmanlab/orf_calling:latest
-
-#I activated already built conda env....not sure if this is a good idea
-#orf-calling is already an env on Rivanna for me, so it wouldn't let me build a new one
-#if this doesn't work, delete that env and build new from container 
-conda activate orf-calling 
-
-##BE SURE TO DO THIS BEFORE OPENING THE APPTAINER SO YOU CAN WRITE OUTPUT FILES
-apptainer exec --bind $SCRATCH:/scratch/yqy3cu/LRP_tutorial/Visualization orf_calling_latest.sif ls /scratch
-
-#now be sure your working directory on Rivanna is in your scratch...otherwise you won't be able to write output files
+```
+After pulling the docker, create and activate conda environment
+```
+conda env create -f ./00_environments/orf_calling.yml
+conda activate orf-calling
+pip install gtfparse
+```
+Add the apptainer to the scratch folder before entering the apptainer and make sure that your working directory is in scratch so you can write output files.
+```
+apptainer exec --bind $SCRATCH:/project/sheynkman/users/emily/LRP_test/jurkat orf_calling_latest.sif ls /project/sheynkman/users/emily/LRP_test/jurkat
 apptainer exec orf_calling_latest.sif ls $SCRATCH
+```
 
-#now enter the containter 
+Now enter the containter and run the script that is on Rivanna (Apptainer files are read only)
+```
 apptainer exec orf_calling_latest.sif /bin/bash
 
-#run script that is on Rivanna (APPTAINER FILES ARE READ ONLY)
-python /scratch/yqy3cu/ORF_docker/orf_calling.py \
---orf_coord /scratch/yqy3cu/ORF_docker/jurkat_chr22.ORF_prob.tsv \
---orf_fasta /scratch/yqy3cu/ORF_docker/jurkat_chr22.ORF_seqs.fa \
---gencode /scratch/yqy3cu/ORF_docker/gencode.v35.annotation.chr22.gtf \
---sample_gtf /scratch/yqy3cu/ORF_docker/jurkat_chr22_corrected.gtf \
---pb_gene /scratch/yqy3cu/ORF_docker/pb_gene.tsv \
---classification /scratch/yqy3cu/ORF_docker/jurkat_chr22_classification.txt \
---sample_fasta /scratch/yqy3cu/ORF_docker/jurkat_chr22_corrected.fasta \
---output /scratch/yqy3cu/ORF_docker/jurkat_best_ORF.tsv
-
-
-
-python orf_calling.py \
---orf_coord cpat_out_dir.ORF_prob.tsv \
---orf_fasta cpat_out_dir.ORF_seqs.fa \
---gencode gencode.v35.annotation.canonical.gtf \
---sample_gtf jurkat_corrected.gtf \
---pb_gene jurkat_gene_kallisto.tsv \
---classification jurkat_classification.txt \
---sample_fasta jurkat_corrected.fasta \
---output jurkat_best_ORF.tsv
+python ./00_scripts/04_orf_calling.py \
+--orf_coord ./03_CPAT/cpat.ORF_prob.tsv \
+--orf_fasta ./03_CPAT/cpat.ORF_seqs.fa \
+--gencode ./00_input_data/gencode.v35.annotation.canonical.gtf \
+--sample_gtf ./02_sqanti/output/jurkat_corrected.gtf \
+--pb_gene ./03_transcriptome_summary/pb_gene.tsv \
+--classification ./02_sqanti/output/jurkat_classification.txt \
+--sample_fasta ./02_sqanti/output/jurkat_corrected.fasta \
+--output ./04_orf_calling/jurkat_best_ORF.tsv
+```
