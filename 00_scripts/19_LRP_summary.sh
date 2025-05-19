41feec04f03cfcf22416be9daffb0ec4e937f9e9
Emily F. Watts <watts.emily.f@gmail.com>
2025-05-19 15:14:13 -0400

    Update 19_LRP_summary.sh

--- 00_scripts/19_LRP_summary.sh
+++ 00_scripts/19_LRP_summary.sh
@@ -23,12 +23,6 @@ module load R/4.4.1
 
 conda activate reference_tab
 
-# Step 0 - make gene counts from isoseq
-python sum_gene_cpm.py \
-  -i input_cpm.tsv \
-  -o output_gene_cpm.tsv \
-  -s sample1_name,sample2_name,sample3_name,sample4_name,sample5_name,sample6_name \
-
 # Step 1 - align CDS_GTF file with SQANTI classification output (need CPM for each biological sample, and need each biological sample to be renamed as X504_Q157R, A258_Q157R, A309_Q157R, V335_WT, V334_WT, and A310_WT) and gene name and transcript from sqanti_isoform_info.tsv
 python 00_scripts/19_summary_table.py -s 03_filter_sqanti/sample_classification.5degfilter.tsv -w 07_make_cds_gtf/condition1_cds.gtf -m 07_make_cds_gtf/condition2_cds.gtf -i 04_transcriptome_summary/sqanti_isoform_info.tsv -o 19_LRP_summary/full_summary.tsv
 
@@ -36,7 +30,7 @@ python 00_scripts/19_summary_table.py -s 03_filter_sqanti/sample_classification.
 python 00_scripts/19_sum_gene_cpm.py -i 19_LRP_summary/full_summary.tsv -o 19_LRP_summary/gene_cpm_summary.tsv
 
 # Step 3 - isoform fractional abundance table with transcript name and CPM for each biological sample
-python 00_scripts/19_calculate_isoform_fractions.py -i 19_LRP_summary/full_summary.tsv -o 19_LRP_summary/isoform_fractions.tsv
+python 00_scripts/19_calculate_isoform_fractions.py -i 19_LRP_summary/full_summary.tsv -o 19_LRP_summary/isoform_fractions.tsv -s sample1_name,sample2_name,sample3_name,sample4_name,sample5_name,sample6_name
 
 # Step 4 - differential expression 
     # bring transcript counts table to edgeR and generate differential gene expression with average WT and mutant samples with p-values
